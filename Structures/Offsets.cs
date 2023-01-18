d985820d420f8895917a0c263642f4991b1e9162
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2023-01-18 20:19:32 +1100

    Update offsets for 2.1.0

--- Structures/Offsets.cs
+++ Structures/Offsets.cs
@@ -2,8 +2,8 @@
 {
     internal class Offsets
     {
-        public const uint GearList_Head = 0xBE3415B0;
-        public const uint GearList_Clothes = 0xBE36D5D0;
-        public const uint GearList_Shoes = 0xBE3995F0;
+        public const uint GearList_Head = 0xBE3455B0;
+        public const uint GearList_Clothes = 0xBE3715D0;
+        public const uint GearList_Shoes = 0xBE39D5F0;
     }
 }
