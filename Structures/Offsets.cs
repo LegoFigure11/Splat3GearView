49aa902e04c542693ba8a9ff0e7991163640fdca
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2023-11-30 20:46:28 +1100

    6.0.0

--- Structures/Offsets.cs
+++ Structures/Offsets.cs
@@ -3,10 +3,10 @@
     internal class Offsets
     {
         private const uint GearList_Offs = 0x2C020;
-        public const uint GearList_Head = 0xBCD55B30;
+        public const uint GearList_Head = 0xBCE9D728;
         public const uint GearList_Clothes = GearList_Head + GearList_Offs;
         public const uint GearList_Shoes = GearList_Clothes + GearList_Offs;
 
-        public const uint GachaBlock = 0xBCE3B998;
+        public const uint GachaBlock = 0xBCF8B390;
     }
 }
