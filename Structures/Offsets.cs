c283fea398746dab431aa51ab82bfadfcc482f3d
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2025-09-30 13:55:23 +1000

    S3 v10.1.0

--- Structures/Offsets.cs
+++ Structures/Offsets.cs
@@ -5,10 +5,10 @@
         public const uint DLC_Offs = 0x3B000;
 
         private const uint GearList_Offs = 0x2C020;
-        public const uint GearList_Head = 0xB9F47F00;
+        public const uint GearList_Head = 0xBA304DD8;
         public const uint GearList_Clothes = GearList_Head + GearList_Offs;
         public const uint GearList_Shoes = GearList_Clothes + GearList_Offs;
 
-        public const uint GachaBlock = 0xBA03EE50;
+        public const uint GachaBlock = 0xBA3FBD28;
     }
 }
